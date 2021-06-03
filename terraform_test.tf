2a12cb1ebdb5951af8c6dfa2357fb6a3946f7421
martineduvaz <vazquezmartinn@gmail.com>
2021-06-03 14:07:50 -0300

    modified stuff

--- terraform_test.tf
+++ terraform_test.tf
@@ -1,9 +1,8 @@
 provider "aws" {
   region     = "us-east-2"
-  access_key = "acces_key"
-  secret_key = "ecret_key"
+  access_key = "inster_key"
+  secret_key = "insert_secret"
 }
-
 
 resource "aws_instance" "us-east2" {
   ami           = "ami-00399ec92321828f5"
